fd34eb508819356bd9a2bbb0774a57bc3b81faa9
Charles Zawacki <cazwacki@vt.edu>
2024-03-19 23:58:14 -0400

    feat: logging for cw debugging

--- functions/caller/app.py
+++ functions/caller/app.py
@@ -9,6 +9,7 @@ def lambda_handler(event, context):
     destination_phone_number = event.get('destination_phone_number', None)
 
     if(None in [taskie_id, message, destination_phone_number]):
+        print(f'lambda_handler: improperly formatted event input; taskie_id = {taskie_id}, message = {message}, destination_phone_number = {destination_phone_number}')
         return {
             'statusCode': 400,
             'body': 'inputs "taskie_id", "message", "destination_phone_number" required'
@@ -20,12 +21,14 @@ def lambda_handler(event, context):
         instance_id = ssm_client.get_parameter(Name='INSTANCE_ID')['Parameter']['Value']
         source_phone_number = ssm_client.get_parameter(Name='SOURCE_PHONE_NUMBER')['Parameter']['Value']
     except botocore.exceptions.ClientError as error:
+        print('lambda_handler: failed to retrieve parameters from ssm')
         return {
             'statusCode': 500,
-            'body': f'{error.response["Error"]["Code"]}; failed to retrieve parameters'
+            'body': f'{error.response["Error"]["Code"]}; failed to retrieve parameters from ssm'
         }
 
     if(None in [contact_flow_id, instance_id, source_phone_number]):
+        print(f'lambda_handler: improper ssm parameters; contact_flow_id = {contact_flow_id}, instance_id = {instance_id}, source_phone_number = {source_phone_number}')
         return {
             'statusCode': 503,
             'body': 'parameters "CONTACT_FLOW_ID", "INSTANCE_ID", "SOURCE_PHONE_NUMBER" required'
@@ -43,12 +46,14 @@ def lambda_handler(event, context):
             }
         )
     except botocore.exceptions.ClientError as error:
+        print(f'{error.response["Error"]["Code"]}; failed to start outbound call')
         return {
             'statusCode': 500,
             'body': f'{error.response["Error"]["Code"]}; failed to start outbound call'
         }
 
     contact_id = response.get('ContactId', 'not provided')
+    print(f'sent taskie {taskie_id} to {destination_phone_number}; contact id is {contact_id}')
     return {
         'statusCode': 200,
         'body': f'sent taskie {taskie_id} to {destination_phone_number}; contact id is {contact_id}'
