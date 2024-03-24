2c7bed7294a26cefafb1606ab8e56346e3d4127b
Charles Zawacki <cazwacki@vt.edu>
2024-03-24 12:12:46 -0400

    feat: shell for home page

--- site/src/pages/Home.js
+++ site/src/pages/Home.js
@@ -1,5 +1,9 @@
 const Home = () => {
-    return <h1>Home</h1>;
+    return <div className="text-center">
+        <img className="m-auto" alt="Taskie Logo" src="/taskie.png" />
+        <h1>Taskie</h1>
+        <h1>Set reminders and follow-up phone calls for recurring tasks</h1>
+    </div>;
 };
 
 export default Home;
