aee822f447246b75a36c06a668f0626ebd92e22d
Charles Zawacki <cazwacki@vt.edu>
2024-03-24 12:29:21 -0400

    feat: home page art

--- site/src/pages/Home.js
+++ site/src/pages/Home.js
@@ -1,7 +1,9 @@
 const Home = () => {
     return <div className="text-center">
         <img className="m-auto" alt="Taskie Logo" src="/taskie.png" />
-        <h1>Taskie</h1>
+        <br></br>
+        <img className="m-auto" alt="Taskie Text" src="/taskie-text.png" />
+        <br></br>
         <h1>Set reminders and follow-up phone calls for recurring tasks</h1>
     </div>;
 };
